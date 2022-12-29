642d1ea1ffe9d1a30a524b004c8d572749820726
eelli002 <elijah_elliott@hotmail.com>
2022-12-28 23:39:55 -0800

    DB Creation & Connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
-const express = require("express");
-const cors = require("cors")
+const express = require('express');
+const cors = require('cors')
 const app = express();
+const pool = require('./db');
 
-// Middleware
+/**** MIDDLEWARE ****/
 app.use(cors());
-
 app.use(express.json());
+
+
+/**** ROUTES ****/
+
+// Create a todo
+app.post('/todos', async(req, res) => {
+    try 
+    {
+        console.log(req.body);
+    } 
+    catch (error) 
+    {
+        console.error(error.message);
+    }
+})
+
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo
+
+
 
 app.listen(3000, () => {
     console.log("Server has started on port 3000");
