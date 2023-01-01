07c90d000d0957970346f32569aacf1e072192a8
eelli002 <elijah_elliott@hotmail.com>
2022-12-31 23:31:03 -0800

    InputTodo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React, { Fragment } from "react";
 
+// Components
+import InputTodo from './components/inputTodo';
+
 function App() {
   return (
     <Fragment>
-      
+      <div className='Container'>
+        <InputTodo/>
+      </div>
     </Fragment>
   );
 }
