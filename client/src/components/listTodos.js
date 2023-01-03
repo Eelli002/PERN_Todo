d89edf2b719f60c870a60edf4a954d60dcdd0e40
eelli002 <elijah_elliott@hotmail.com>
2023-01-02 23:05:26 -0800

    Finished the delete component

--- client/src/components/listTodos.js
+++ client/src/components/listTodos.js
@@ -4,6 +4,7 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    // Gets the list of all todos and calls the setState function: setTodos to set the state of todos.
     const getTodos = async() => {
         try 
         {
@@ -17,7 +18,24 @@ const ListTodos = () => {
         }
     }
 
-    useEffect(getTodos, []);
+    // Takes the todo_id of todo item and sends a delete request.
+    const deleteTodo = async(id) => {
+        try 
+        {
+            const deleteTodo = await fetch(`http://localhost:3001/todos/${id}`, {method : 'DELETE'});
+            console.log(deleteTodo);
+            // To update our list without refeshing the page, we can update our todo state to filter out the todo with the deleted id.
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } 
+        catch (error) 
+        {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
 
     return (
         <>
@@ -32,10 +50,17 @@ const ListTodos = () => {
                 </thead>
                 <tbody> 
                     { todos.map(todo => (
-                        <tr>
+                        <tr key = {todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>Edit</td>
-                            <td>Delete</td>
+                            <td>
+                                <button 
+                                    className='btn btn-danger' 
+                                    onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
                         </tr> 
                     ))} 
                 </tbody>
