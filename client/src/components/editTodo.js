f61d383bec521898828d12e751ae8a231bde0397
eelli002 <elijah_elliott@hotmail.com>
2023-01-03 14:38:02 -0800

    Edit functionality finished

--- client/src/components/editTodo.js
+++ client/src/components/editTodo.js
@@ -26,7 +26,7 @@ const EditTodo = ({todo}) => {
     return (
         <>
             {/* Modal code from W3 school*/}
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target={`#id${todo.todo_id}` } onClick={() => setDescription(todo.description)}>
             Edit
             </button>
 
@@ -36,7 +36,7 @@ const EditTodo = ({todo}) => {
 
                 <div className="modal-header">
                     <h4 className="modal-title">Edit Todo</h4>
-                    <button type="button" className="close" data-dismiss="modal">&times;</button>
+                    <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
                 </div>
 
                 <div className="modal-body">
@@ -45,7 +45,7 @@ const EditTodo = ({todo}) => {
 
                 <div className="modal-footer">
                     <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={e => updateDescription(e)}>Edit</button>
-                    <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
                 </div>
 
                 </div>
